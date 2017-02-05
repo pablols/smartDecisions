9a6d18ca2553028b36da0c9241295ebf3cc32d58
Pablo Larraguibel Ch <plarraguibelch@gmail.com>
2017-02-05 20:22:50 -0300

    Update get_trello.py

--- Trello_Dashboard/get_trello.py
+++ Trello_Dashboard/get_trello.py
@@ -16,50 +16,51 @@ from trolly import ResourceUnavailable
 #Pablo recuerda comentar tus codigos !
 #Editado y subido command line por GIT
 
-
+# la api_key y user_auth_token son obtenidos en Trello y son unicos por usuario, 
+# estas son como las contraseñas de la API de trello.
+# Ambos valores son ocupados por la libreria para conectarse a la API
 api_key = 'c55e4cc78fed9752274056a24a5bbbf7'
 user_auth_token = 'aa7121a7a8cc263c91752c949055901e599890f463a592cb05d878fca47262e7'
 
+# definimos la Organisacion a la cual leeremos, en este caso es nuestra organizacion en Trello
 organisation = 'smartdecisions1' #'588d095968a1972b407f2d5c'
+# El board_id corresppnde al ID de algun proyecto que deseemos revisar
 board_id = '588d0bb61aae1a8635b2bd5d'
-list_id = ''
-card_id = ''
-checklist_id = ''
-member_id = ''
 
-def getBoards():
+
+#Con esta funcion , obtenemos los proyectos y sus responsables, recibe como input la organisation, 
+# dejaremos como default nuestra organizacion.
+def getBoards(organisation='smartdecisions1'):
     try:
-        client=Client(api_key, user_auth_token)
-        org=Organisation(client, organisation)
-        for board in org.get_boards():
-            print 'Proyecto: ', board.name
-            print 'Esclavos: '
-            for esclavo in board.get_members():
-                print '          -', esclavo.name
-            print '  '
+       client=Client(api_key, user_auth_token)
+       org=Organisation(client, organisation)
+       for board in org.get_boards():
+           print 'Proyecto: ', board.name
+           print 'Responsables: '
+           for responsable in board.get_members():
+               print '          -', responsable.name
+               print '  '
     except:
         print 'error, plsss revisa tus inputs flojo qliao'
-
-def getCards(board_id=None):
-    if board_id:
-        try:
-            client=Client(api_key, user_auth_token)
-            board=trolly.board.Board(client,board_id)
-            print 'Tareas y sus responsbles'
-            for card in board.get_cards():
-                print 'Tarae: ', card.name
-                print 'Esclavos :'
-                for esclavo in card.get_members():
-                    print '       -',esclavo.name
-                print '¿Tarea Terminada? ', card.get_card_information()['dueComplete']
-                print '              '
-        except:
-            print 'error, plsss revisa tus inputs flojo qliao'
-    else:
-        print 'ingresa un board_id'
+#con esta funcion obtenemos todos los proyectos, sus reponsables y estado de avance. recibe como input el board_id
+def getCards(board_id='588d0bb61aae1a8635b2bd5d'):
+    try:
+        client=Client(api_key, user_auth_token)
+        board=trolly.board.Board(client,board_id)
+        print 'Tareas y sus responsbles'
+        for card in board.get_cards():
+            print 'Tarae: ', card.name
+            print 'Responsables :'
+            for responsable in card.get_members():
+                print '       -',responsable.name
+            print '¿Tarea Terminada? ', card.get_card_information()['dueComplete']
+            print ''
+            print ''
+    except:
+        print 'error, plsss revisa tus inputs flojo qliao'
         
 print ' ------------ Proyectos ------------'          
 getBoards()
 print ''
 print ' ------------ Tareas del Proyecto Tierra ------------'
-getCards(board_id)
+getCards()
